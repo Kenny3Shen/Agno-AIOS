8058adf4b6e5a80f32ace7079a20f3b5432137ff
shenss1 <shenss1@xiaopeng.com>
2026-01-06 11:34:37 +0800

    - 更新前端资源

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,10 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    outDir: '../source', // 设置输出目录为 ../source
+    emptyOutDir: true, // 构建前清空输出目录
+  },
   server: {
     proxy: {
       '/api': {
